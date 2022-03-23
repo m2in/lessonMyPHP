125759c4f904803bee39c0181c2d8652252c5134
M@Ms <35535283+m2in@users.noreply.github.com>
2022-03-23 16:28:22 +0300

    Add files via upload

---
+++ blocks/asside.php
@@ -0,0 +1,13 @@
+<aside class="col-md-4">
+	<div class="p-3 mb-3 bg-warning rounded">
+		<h4>
+			<b>
+				Интересные факты
+			</b>
+		</h4>
+		<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni autem consequuntur veniam esse sapiente? Assumenda ab perferendis quos, perspiciatis eos itaque quisquam, corrupti temporibus esse numquam placeat praesentium alias suscipit.</p>
+	</div>
+	<div class="p-3 mb-3">
+		<img class="img-thumbnail" src="https://itproger.com/img/courses/1534230100.jpg" alt="">
+	</div>
+</aside>
---
+++ blocks/header.php
@@ -0,0 +1,26 @@
+<header>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">MySitePHP</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<ul class="navbar-nav me-auto mb-2 mb-md-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Link0</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link1</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link">Link2</a>
+					</li>
+				</ul>
+				<a class="btn btn-outline-primary me-3" href="#">Войти</a>
+				<a class="btn btn-outline-primary" href="#">Регистрация</a>
+				
+			</div>
+		</div>
+	</nav>
+</header>
