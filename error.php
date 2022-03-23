eacd0385680de05df7127d60e9101d93a0360a59
M@Ms <35535283+m2in@users.noreply.github.com>
2022-03-23 15:04:38 +0300

    Add files via upload

---
+++ error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ошибка 404</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+</head>
+<body>
+	<?php
+		echo '<h1>Ошибка 404 - Страница НЕ найдена - перейдите <a href="/">на  Главную страницу сайта</a></h1>'
+	?>
+</body>
+</html>
