abd1ec541b45cf6dcf1ab48451502b5d7301db84
M@Ms <35535283+m2in@users.noreply.github.com>
2022-03-23 16:36:42 +0300

    Update error.php

--- error.php
+++ error.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Ошибка 404</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
-</head>
-<body>
-	<?php
-		echo '<h1>Ошибка 404 - Страница НЕ найдена - перейдите <a href="/">на  Главную страницу сайта</a></h1>'
-	?>
-</body>
+	<?php require 'blocks/headInst.php'; ?>
+	<body>
+		<?php require 'blocks/header.php'; ?>
+		<main class="container mt-5">
+			<div class="row pt-5">
+				<div class="col-md-8">
+					<?php echo '<h1><strong>Ошибка 404</strong><br>- Страница НЕ найдена <br>- перейдите <a href="/">на  Главную страницу сайта</a></h1>'; ?>
+				</div>
+				<?php require 'blocks/asside.php'; ?>
+			</div>
+		</main>
+		
+		<?php require 'blocks/footer.php'; ?>
+	</body>
 </html>
