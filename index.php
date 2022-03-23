c1fd343bac76efb8631327b85bf0761b8a792c36
M@Ms <35535283+m2in@users.noreply.github.com>
2022-03-23 16:37:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Lesson PHP | My Test site</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
-</head>
-<body>
-	<?php
-	echo "Проверка"
-	?>
-</body>
+	<?php require 'blocks/headInst.php'; ?>
+	<body>
+		<?php require 'blocks/header.php'; ?>
+
+		<main class="container mt-5">
+			<div class="row pt-5">
+				<div class="col-md-8">
+					Основная часть сайта
+				</div>
+				<?php require 'blocks/asside.php'; ?>
+			</div>
+		</main>
+
+		<?php require 'blocks/footer.php'; ?>
+	</body>
 </html>
